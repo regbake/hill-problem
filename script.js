eb51315325d564d04883ce607347e77bce81668c
regbake <regbake@gmail.com>
2018-06-19 11:20:20 -0700

    balancing the vehicle

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var angle = 0;
+var vehicleX = 75;
+var isSlopePositive = undefined;
 // var incline = document.getElementsByClassName("incline")[0];
+
+//interval to keep checking for inflection
+setInterval(checkAngle, 1000);
 
 function addAngle(){
   var incline = document.getElementsByClassName("system")[0];
@@ -19,4 +24,21 @@ function lessAngle(){
 
 function updateAngle() {
   document.getElementById("current-angle").innerHTML = angle;
+}
+
+//set the angle to pos or negative
+function checkAngle() {
+  if (angle > 0){
+    isSlopePositive = true;
+
+    vehicleX++;
+    document.getElementsByClassName("vehicle")[0].style.left = vehicleX + "px";
+  }
+  else {
+    isSlopePositive = false;
+
+    vehicleX--;
+    document.getElementsByClassName("vehicle")[0].style.left = vehicleX + "px";
+  }
+
 }
